57dff514b298eaff132def4cf38eb8049751aa85
Scott Alfter <scott@alfter.us>
2016-09-21 15:03:27 -0700

    some *coin updates

--- net-p2p/make-coin-ebuild-autotools.sh
+++ net-p2p/make-coin-ebuild-autotools.sh
@@ -3,12 +3,12 @@ lower=`echo $1 | awk '{print tolower($0)}'`
 upper=`echo $1 | awk '{print toupper($0)}'`
 mixed=`echo $1 | awk '{print substr(toupper($0),1,1)substr(tolower($0),2,length($0)-1)}'`
 
-cp -r dogecoind ${lower}d
+cp -r digitalcoind ${lower}d
 cd ${lower}d
-for i in dogecoind-9999.ebuild files/*
+for i in digitalcoind-9999.ebuild files/*
 do 
-  mv $i `echo $i | sed "s/dogecoin/${lower}/"`
-  sed -i "s/dogecoin/${lower}/g;s/Dogecoin/${mixed}/g;s/DOGECOIN/${upper}/g" `echo $i | sed "s/dogecoin/${lower}/"`
+  mv $i `echo $i | sed "s/digitalcoin/${lower}/"`
+  sed -i "s/digitalcoin/${lower}/g;s/Digitalcoin/${mixed}/g;s/DIGITALCOIN/${upper}/g" `echo $i | sed "s/digitalcoin/${lower}/"`
 done
 f2=$(echo $2 | sed "s/\//\\\\\//g")
 sed -i "s/^HOMEPAGE=.*/HOMEPAGE=\"https:\/\/github.com\/${f2}\"/;s/^EGIT_REPO_URI=.*/EGIT_REPO_URI=\"https:\/\/github.com\/${f2}\"/;s/DB_VER=\"5.1\"/DB_VER=\"4.8\"/" *.ebuild
@@ -16,12 +16,12 @@ ebuild *.ebuild digest
 
 cd ..
 
-cp -r dogecoin-cli ${lower}-cli
+cp -r digitalcoin-cli ${lower}-cli
 cd ${lower}-cli
-for i in dogecoin-cli-9999.ebuild files/*
+for i in digitalcoin-cli-9999.ebuild files/*
 do 
-  mv $i `echo $i | sed "s/dogecoin/${lower}/"`
-  sed -i "s/dogecoin/${lower}/g;s/Dogecoin/${mixed}/g;s/DOGECOIN/${upper}/g" `echo $i | sed "s/dogecoin/${lower}/"`
+  mv $i `echo $i | sed "s/digitalcoin/${lower}/"`
+  sed -i "s/digitalcoin/${lower}/g;s/Digitalcoin/${mixed}/g;s/DIGITALCOIN/${upper}/g" `echo $i | sed "s/digitalcoin/${lower}/"`
 done
 f2=$(echo $2 | sed "s/\//\\\\\//g")
 sed -i "s/^HOMEPAGE=.*/HOMEPAGE=\"https:\/\/github.com\/${f2}\"/;s/^EGIT_REPO_URI=.*/EGIT_REPO_URI=\"https:\/\/github.com\/${f2}\"/" *.ebuild
